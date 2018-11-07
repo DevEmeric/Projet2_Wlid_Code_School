9e0c9af6e99704319a211f60197e6a79474ef4c1
Mathieu De Armey <mdearmey@gmail.com>
2018-11-07 09:32:46 +0100

    Pause

--- src/scripts/wallFight.js
+++ src/scripts/wallFight.js
@@ -4,55 +4,6 @@ import background1 from '../image/background/backgroundCastle.jpg';
 import background2 from '../image/background/backgroundCastleBasement.jpg';
 import background3 from '../image/background/backgroundCastleDoor.jpg';
 import background4 from '../image/background/backgroundCastleFire.jpg';
-<<<<<<< HEAD
-import background5 from '../image/background/backgroundCastleNight.jpg';
-import background6 from '../image/background/backgroundCastleStairs.jpg';
-import background7 from '../image/background/backgroundForestLightning.jpg';
-import background8 from '../image/background/backgroundQuidditch.jpg';
-import background9 from '../image/background/backgroundQuidditchFight.jpg';
-import background10 from '../image/background/backgroundQuidditchStadium.jpg';
-import background11 from '../image/background/backgroundTrain.jpg';
-
-class Wall extends Component {
-  constructor() {
-    super();
-    this.state = {
-      bgImage: [
-        background1,
-        background2,
-        background3,
-        background4,
-        background5,
-        background6,
-        background7,
-        background8,
-        background9,
-        background10,
-        background11,
-      ],
-      selectedImage: background1,
-    };
-  }
-
-  componentDidMount() {
-    this.getRandomBackground();
-  }
-
-  getRandomBackground() {
-    const randomImage = this.state.bgImage[Math.floor(Math.random() * this.state.bgImage.length)];
-    this.setState({
-      selectedImage: randomImage,
-    });
-  }
-
-  render() {
-    return (
-      <div className="pic" style={{ backgroundImage: `url(${this.state.selectedImage})` }} />
-    );
-  }
-}
-
-=======
 import background5 from '../image/background/backgroundCastleIndoor.jpg';
 import background6 from '../image/background/backgroundCastleNight.jpg';
 import background7 from '../image/background/backgroundCastleStairs.jpg';
@@ -103,5 +54,4 @@ class Wall extends Component {
     }
 }
 
->>>>>>> 65e030c205e36906bb8b32219760409e2cb0f5cc
 export default Wall;
