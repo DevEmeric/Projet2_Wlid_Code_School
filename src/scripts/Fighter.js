4ac323472d5a9df562c058c071b03e49d7027c78
Emeric <39947703+DevEmeric@users.noreply.github.com>
2019-05-01 22:40:45 +0200

    Update Fighter.js

--- src/scripts/Fighter.js
+++ src/scripts/Fighter.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import './Fighter.css';
+import GryffindorColor from "../image/silouhetteGryffindor.png";
+import SlytherinColor from "../image/silouhetteSlytherin.png";
+import RavenclawColor from "../image/silouhetteRavenclaw.png";
+import HufflepuffColorColor from "../image/silouhetteHufflepuff.png";
+//import HeadDeath from "../image/tetedemort.png"; Image tête de mort si besoin
+import HeadDeath2 from "../image/lightning-death.png";
+import Impact from "../image/Impact.gif"
 
 class Fighter extends Component {
 
   constructor(props) {
     super(props);
+    this.Gryffindor = GryffindorColor;
+    this.Slytherin = SlytherinColor;
+    this.Ravenclaw = RavenclawColor;
+    this.Hufflepuff = HufflepuffColorColor;
+    //this.Head = HeadDeath; image tête de mort si besoin
+    this.Head2 = HeadDeath2;
+
     this.state = {
       spellCasted: false,
       rotation: this.props.fighter.rotation,
@@ -13,48 +27,100 @@ class Fighter extends Component {
       left: this.props.fighter.left,
       width: this.props.fighter.width,
       height: this.props.fighter.height,
-      speed: 5,
+      speed: 50,
+      tabKeys: [],
     }
   }
 
-  handleKeyPress = (event) => {
-    switch (event.which) {
-      case this.props.fighter.attack: this.props.fighter.castSpell(this.props.fighter.id, this.props.fighter.facesRight); break;
-      //case this.props.fighter.defense : this.props.fighter.defend(this.props.fighter.id); break;
-      case this.props.fighter.rotate: this.props.fighter.rotateFighter(this.props.fighter.id); break;
-      case this.props.fighter.moveUp: this.props.fighter.move(this.props.fighter.id, -this.state.speed, 0); break;
-      case this.props.fighter.moveDown: this.props.fighter.move(this.props.fighter.id, this.state.speed, 0); break;
-      case this.props.fighter.moveLeft: this.props.fighter.move(this.props.fighter.id, 0, -this.state.speed); break;
-      case this.props.fighter.moveRight: this.props.fighter.move(this.props.fighter.id, 0, this.state.speed); break;
-    }
 
+  componentDidMount() {
+
+    document.addEventListener("keydown", this.inTab1, false)
+    document.addEventListener("keyup", this.outTab1, false);
+
+  }
+
+
+
+  inTab1 = (event) => {
+    let localTabKeys = this.state.tabKeys;
+    if (localTabKeys.indexOf(event.key) < 0) {
+      localTabKeys.push(event.key)
+    }
     this.setState({
-      spellCasted: this.props.fighter.spellCasted,
-      top: this.props.fighter.top,
-      left: this.props.fighter.left,
-      rotation: this.props.fighter.rotation
+      tabKeys: localTabKeys,
+    })
+    this.handleKeyPress(localTabKeys)
+  }
+
+
+  handleKeyPress = (localTabKeys) => {
+    if (this.props.victory === false && this.props.displayInstr === false && this.props.startGame === false) {
+      if (localTabKeys.indexOf(this.props.fighter.moveUp) !== -1 && this.props.fighter.top > 110) {
+        this.props.fighter.move(this.props.fighter.id, -this.state.speed, 0)
+      }
+      if (localTabKeys.indexOf(this.props.fighter.moveDown) !== -1 && this.props.fighter.top < window.innerHeight - this.props.fighter.height - 20) {
+        this.props.fighter.move(this.props.fighter.id, this.state.speed, 0)
+      }
+      if (localTabKeys.indexOf(this.props.fighter.moveLeft) !== -1 && this.props.fighter.left > -20) {
+        this.props.fighter.move(this.props.fighter.id, 0, -this.state.speed)
+      }
+      if (localTabKeys.indexOf(this.props.fighter.moveRight) !== -1 && this.props.fighter.left < window.innerWidth - this.props.fighter.width - 20) {
+        this.props.fighter.move(this.props.fighter.id, 0, this.state.speed)
+      }
+      if (localTabKeys.indexOf(this.props.fighter.attack) !== -1) {
+        this.props.fighter.castSpell(this.props.fighter.id, this.props.fighter.facesRight)
+      }
+      if (localTabKeys.indexOf(this.props.fighter.rotate) !== -1) {
+        this.props.fighter.rotateFighter(this.props.fighter.id)
+      }
+      if (localTabKeys.indexOf(this.props.fighter.defend) !== -1 && this.props.fighter.defense.shieldNumber > 0) {
+        this.props.fighter.takeOutShield(this.props.fighter.id)
+      }
+    }
+  }
+
+
+
+  outTab1 = (event) => {
+    let localTabKeys = this.state.tabKeys;
+    if (localTabKeys.indexOf(event.key) >= 0) {
+      localTabKeys.splice(localTabKeys.indexOf(event.key), 1)
+    }
+    this.setState({
+      tabKeys: localTabKeys,
     })
   }
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress, false);
-  }
+
 
   render() {
+
     let fighterStyle = {
-      transform: `rotateY(${this.state.rotation}deg)`,
+      transform: `rotateY(${this.props.fighter.rotation}deg)`,
       position: "absolute",
-      top: this.state.top + "px",
-      left: this.state.left + "px",
+      top: this.props.fighter.top + "px",
+      left: this.props.fighter.left + "px",
       width: this.state.width + "px",
       height: this.state.height + "px",
+      backgroundImage: this.props.fighter.deathFighter === true ? `url(${Impact})` : `url(${this[this.props.fighter.house]})`,
+    };// ternaire pour afficher la mort de fightherID
+
+    let fighterStyleTouched = {
+      transform: `rotateY(${this.props.fighter.rotation}deg)`,
+      position: "absolute",
+      top: this.props.fighter.top + "px",
+      left: this.props.fighter.left + "px",
+      width: this.state.width + "px",
+      height: this.state.height + "px",
+      backgroundImage: `url(${this.Head2})`,
     };
 
     let fighterId = "fighter" + this.props.fighter.house
-
     return (
+
       <div>
-        <div className="fighter" style={fighterStyle} id={fighterId}>
+        <div className="fighter" style={(this.props.fighter.touched) ? fighterStyleTouched : fighterStyle} id={fighterId}>
         </div>
       </div>
     );
